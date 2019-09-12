21c98fb2d263ddc5b187af6092924c7304e427be
JackHilliard <47607167+JackHilliard@users.noreply.github.com>
2019-09-12 11:05:01 +0100

    Class for Beacons and RPi Service
    + class for each beacon
    + config file to load each beacon mac address and name

--- bluez_sw_beacon.py
+++ bluez_sw_beacon.py
@@ -1,10 +1,8 @@
 """
-
 References:
 1. Ruuvi Project.
 https://github.com/ttu/ruuvitag-sensor/
 2.bluey_beacon.py from electronut
-
 """
 
 import re
@@ -14,7 +12,7 @@ import time
 import subprocess
 import requests
 import arrow
-import numpy as np
+import json
 
 # zone limit and sizes
 # -20 to -30 at beacon
@@ -22,9 +20,7 @@ import numpy as np
 # -65 ~3 metres away
 # very dependant on devices nearby and elevation compared to RPi
 zoneLimit = -65
-
-#URL definition
-url = "http://192.168.1.242:3000"
+rpiZone = "one"
 
 #decode rssi
 def twos_comp(val, bits):
@@ -33,7 +29,7 @@ def twos_comp(val, bits):
     return val
 
 def sendToServer(payload):
-    r=requests.post(url + '/beacondata',
+    r=requests.post(conf["url"] + '/beacondata',
                     headers={'Content-Type': 'application/json'},
                     json=payload)
 
@@ -86,27 +82,31 @@ class BLEScanner:
             print(ex)
             return
 
+class BLEBeacon:
+    def __init__(self, macAddr, name)
+        self.macAddr = macAddr
+        self.name = name
+        self.combRssi = 0
+        self.countRssi = 0
+        self.avgRssi = 0
+        self.zone = False
 
 # main() function
 def main():
-    # use sys.argv if needed
-    #if len(sys.argv) < 2:
-    #    print('Usage: python bluey-beacon.py MACADDR')
-    #    exit(0)
-    #print('starting...')
-
-    #deviceId = sys.argv[1]
     scanner = BLEScanner()
     scanner.start()
-
-    zoneOne = False
+    #beacons = [BLEBeacon("E9:F6:A1:2B:AD:69",u'2BAD69BB'),BLEBeacon("FC:3E:28:A4:8F:4E", u'A48F4EBA')]
+    beacons = []
+    for x in range(len(conf["beacons"])):
+        beacons[x] = BLEBeacon(conf["beacons"][x]["macAddr"],conf["beacons"][x]["name"])
     data = None
     while True:
         try:
             currentTime = time.time()
             if ((round(currentTime) % 10) == 0):
-                print(currentTime)
-                avgRssi = np.zeros((2,len(beacons[0][])))
+                #print(currentTime)
+                for x in range(len(beacons))
+                    beacons[x].combRssi = 0
                 while (time.time() <= currentTime + 15):
                     for line in scanner.get_lines():
                         if line:
@@ -116,36 +116,35 @@ def main():
                             mac = ':'.join(a+b for a,b in zip(reversed_mac[::2], reversed_mac[1::2]))
                             data = line[26:]
                             #cycle through all the known beacons
-                            for x in len(beacons[0][]):
-                                if mac == beacons[0][x] and len(data) == 64:
+                            for x in len(beacons):
+                                if mac == beacons[x].macAddr and len(data) == 64:
                                     #print(mac, data)
-                                    if beacons[1][x] in data:
+                                    if beacons[x].name in data:
                                         #average reading
-                                        #check if it is within range
-                                        avgRssi[0][x]+=twos_comp(int(data[62:],16), 8)
-                                        avgRssi[1][x]+=1
+                                        beacons[x].combRssi+=twos_comp(int(data[62:],16), 8)
+                                        beacons[x].countRssi+=1
                                         break
-                                elif(time.time() >= currentTime + 5): #if it goes 10 seconds without finding any data break
-                                    break;
-                for x in len(beacons[0][]):
-                    if (avgRssi[0][x] == 0):
-                        if(zoneOne==True):
-                            print("Beacon", beacons[0][x], "is now out of range")
-                            sendToServer({'beacon': beacons[0][x], 'zone': 'none', 'timestamp': str(arrow.utcnow())})
-                        zoneOne = False
-                        print("Beacon", beacons[0][x], "cannot be found")
+                                elif(time.time() >= currentTime + 5): #if it goes 5 seconds without finding any data timeout
+                                    break
+                for x in len(beacons):
+                    if (beacons[x].countRssi == 0):
+                        if(beacons[x].zone==True):
+                            print("Beacon", beacons[x].macAddr, "is now out of range")
+                            sendToServer({'beacon': beacons[x].macAddr, 'zone': 'none', 'rssi': 0, 'timestamp': str(arrow.utcnow())})
+                        else: print("Beacon", beacons[x].macAddr, "cannot be found")
+                        beacons[x].zone = False
                     else:
-                        rssi = avgRssi[0][x]/avgRssi[1][x]
+                        beacons[x].avgRssi = beacons[x].combRssi/beacons[x].countRssi
                         #handles beacon leaving or entering zone
-                        if ((rssi >= zoneLimit) and (zoneOne == False)):
-                            zoneOne=True
-                            print("Beacon", beacons[0][x], "is in range")
-                            print("RSSI :", rssi)
-                            sendToServer({'beacon': beacons[0][x], 'zone': 'one', 'timestamp': str(arrow.utcnow())})
-                        elif ((rssi < zoneLimit) and (zoneOne == True)):
-                            zoneOne = False
-                            print("Beacon", beacons[0][x], "is now out of range")
-                            sendToServer({'beacon': beacons[0][x], 'zone': 'none', 'timestamp': str(arrow.utcnow())})
+                        if ((beacons[x].avgRssi >= zoneLimit) and (beacons[x].zone == False)):
+                            beacons[x].zone=True
+                            print("Beacon", beacons[x].macAddr, "is in range")
+                            print("RSSI :", beacons[x].rssi)
+                            sendToServer({'beacon': beacons[x].macAddr, 'zone': rpiZone, 'rssi': beacons[x].avgRssi, 'timestamp': str(arrow.utcnow())})
+                        elif ((beacons[x].avgRssi < zoneLimit) and (beacons[x].zone == True)):
+                            beacons[x].zone = False
+                            print("Beacon", beacons[x].macAddr, "is now out of range")
+                            sendToServer({'beacon': beacons[x].macAddr, 'zone': 'none', 'rssi': 0, 'timestamp': str(arrow.utcnow())})
         except KeyboardInterrupt as ex:
             print("kbi")
             scanner.stop()
